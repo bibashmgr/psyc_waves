bc7b295beb37f22cc2fcd64acdfcb81265422aca
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-02-03 14:57:33 +0545

    [setup]: repo

--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className='w-screen h-screen flex justify-center items-center bg-slate-100 dark:bg-slate-900'>
-      <p className='text-black dark:text-white text-2xl font-bold'>Bibash Magar</p>
-    </div>
-  )
-}
